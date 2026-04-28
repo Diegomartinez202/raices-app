33e3bbc09d7d7f85dcbd34af497ba0e7cc51fc01
Diegomartinez202 <diegomartin0102@gmail.com>
2026-04-28 18:39:48 -0500

    Refactor: Eliminación de baseUrl para resolver advertencias de deprecación

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { defineCustomElements } from '@ionic/pwa-elements/loader'
-
-// Registra elementos de Capacitor (Cámara, Galería, etc.)
-defineCustomElements(window);
-
-const app = createApp(App)
-app.mount('#app') // En index.html debe haber un <div id="app"></div>
